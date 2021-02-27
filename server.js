109c3e184beb6771140ea193b0a87d2a19b82fb3
Linux User <leon_xi@163.com>
2021-02-27 02:12:54 +0000

    增加Context Path作为服务参数

--- server.js
+++ server.js
@@ -1,41 +1,50 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
 
-http.createServer(function (req, res) {
-  const filepath = url.parse(req.url, true).pathname;
+const ctxPath = process.env.CONTEXTPATH | "";
 
-  var filename = "."
-  if (filepath.indexOf("/site") != 0) {
-    filename += "/site"
-  }
-  filename += filepath
+http
+  .createServer(function (req, res) {
+    const filepath = url.parse(req.url, true).pathname;
 
-  if (filepath == "/") {
-      filename += "index.html"
-  }
+    var filename = ".";
 
-  // check for only API call
-  if (filename == "./site/getImages") {
-    imageList = fs.readdirSync("site/images")
-    // sort by date modified
-    imageList.sort(function(a, b) {
-        return fs.statSync("site/images/" + b).mtime.getTime() -
-               fs.statSync("site/images/" + a).mtime.getTime();
-    });
+    if (ctxPath && filepath.indexOf(ctxPath) != 0) {
+      filepath = filepath.replace("/" + ctxPath, "");
+    }
+    if (filepath.indexOf("/site") != 0) {
+      filename += "/site";
+    }
+    filename += filepath;
 
-    files = {"images" : imageList}
-    return res.end(JSON.stringify(files));
-  } else {
-    // simple http sever
-    filename = filename.replace("%20", " ");
-    fs.readFile(filename, function(err, data) {
+    if (filepath == "/") {
+      filename += "index.html";
+    }
+
+    // check for only API call
+    if (filename == "./site/getImages") {
+      imageList = fs.readdirSync("site/images");
+      // sort by date modified
+      imageList.sort(function (a, b) {
+        return (
+          fs.statSync("site/images/" + b).mtime.getTime() -
+          fs.statSync("site/images/" + a).mtime.getTime()
+        );
+      });
+
+      files = { images: imageList };
+      return res.end(JSON.stringify(files));
+    } else {
+      // simple http sever
+      filename = filename.replace("%20", " ");
+      fs.readFile(filename, function (err, data) {
         if (err) {
-          res.writeHead(404, {'Content-Type': 'text/html'});
+          res.writeHead(404, { "Content-Type": "text/html" });
           return res.end("404 " + filename + "Not Found");
         }
         return res.end(data);
       });
-  }
-
-}).listen(4000);
+    }
+  })
+  .listen(4000);
