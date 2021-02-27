0336fe36897c7796808df937e462f79ca64bfcf5
Linux User <leon_xi@163.com>
2021-02-27 05:07:30 +0000

    更新viewer.js插件代码

--- site/main.js
+++ site/main.js
@@ -25,15 +25,15 @@ $(document).ready(function () {
         '"/></li>';
     }
   });
-});
 
-// View an image
-const viewer = new Viewer(document.getElementById("image"), {
-  inline: true,
-  viewed() {
-    viewer.zoomTo(1);
-  },
-});
+  // View an image
+  const viewer = new Viewer(document.getElementById("image"), {
+    inline: true,
+    viewed() {
+      viewer.zoomTo(1);
+    },
+  });
 
-// View a list of images
-const gallery = new Viewer(document.getElementById("images"));
+  // View a list of images
+  const gallery = new Viewer(document.getElementById("images"));
+});
