a80c66d467741b7df42282f83b806ef4041b551a
Linux User <leon_xi@163.com>
2021-02-27 02:36:13 +0000

    增加画面请求Context Path参数

--- site/main.js
+++ site/main.js
@@ -1,8 +1,20 @@
-$( document ).ready(function() {
-    $.get("/getImages", function(data) {
-        var images = JSON.parse(data)["images"];
-        for (i = 0; i < images.length; i++) {
-            document.getElementById("imageList").innerHTML += "<a href=\"images/" + images[i] + "\"> <image src=\"images/" + images[i] + "\"/></a>";
-        }
-    });
+$(document).ready(function () {
+  function getContextPath() {
+    var pathName = document.location.pathname;
+    var index = pathName.substr(1).indexOf("/");
+    var result = pathName.substr(0, index + 1);
+    return result;
+  }
+
+  $.get("/" + getContextPath() + "/getImages", function (data) {
+    var images = JSON.parse(data)["images"];
+    for (i = 0; i < images.length; i++) {
+      document.getElementById("imageList").innerHTML +=
+        '<a href="images/' +
+        images[i] +
+        '"> <image src="images/' +
+        images[i] +
+        '"/></a>';
+    }
+  });
 });
